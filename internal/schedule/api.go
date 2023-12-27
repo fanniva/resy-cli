d9845edd3d177782489d7e2190caa9b01a3548a5
fanniva <152999217+fanniva@users.noreply.github.com>
2023-12-27 09:49:04 -0500

    Update api.go

--- internal/schedule/api.go
+++ internal/schedule/api.go
@@ -36,7 +36,7 @@ type searchResponse struct {
 func searchVenues(query string) (*[]surveyVenue, error) {
 	searchRequest := searchRequest{
 		Query:   query,
-		PerPage: 300,
+		PerPage: 600,
 		Types:   []string{"venue"},
 	}
 	body, err := json.Marshal(searchRequest)
