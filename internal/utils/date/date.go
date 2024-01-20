7f2a31f2201c86252d2faaa16f740b743e6fdc5f
fanniva <152999217+fanniva@users.noreply.github.com>
2024-01-20 09:28:28 -0500

    Update date.go

--- internal/utils/date/date.go
+++ internal/utils/date/date.go
@@ -15,7 +15,15 @@ func ParseDate(s string) (*time.Time, error) {
 	return &dateTime, nil
 }
 
-func ParseTime(s string) (*time.Time, error) {
+func ParseTimeS(s string) (*time.Time, error) {
+	dateTime, err := time.ParseInLocation("15:04", s, time.Local)
+	if err != nil {
+		return nil, err
+	}
+
+	return &dateTime, nil
+}
+func ParseTimeL(s string) (*time.Time, error) {
 	dateTime, err := time.ParseInLocation("15:04:05", s, time.Local)
 	if err != nil {
 		return nil, err
@@ -23,7 +31,6 @@ func ParseTime(s string) (*time.Time, error) {
 
 	return &dateTime, nil
 }
-
 func ParseDateTime(s string) (*time.Time, error) {
 	dateTime, err := time.ParseInLocation("2006-01-02 15:04:05", s, time.Local)
 
